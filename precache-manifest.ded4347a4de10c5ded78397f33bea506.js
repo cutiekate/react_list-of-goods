890ef67b6e3d01bfe350849aa0ca7da1b9a23280
kate <kate@test.com>
2020-04-30 01:47:58 +0300

    Updates

---
+++ precache-manifest.ded4347a4de10c5ded78397f33bea506.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c28ada19f70e19c944fe650b503e3c7b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "71969daac52a1a21c83d",
+    "url": "./static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "d56c70eebd3ab8e76cce",
+    "url": "./static/js/2.fe7a5d8b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.fe7a5d8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71969daac52a1a21c83d",
+    "url": "./static/js/main.ab9d6cbd.chunk.js"
+  },
+  {
+    "revision": "5094eb25b1a0c08273e9",
+    "url": "./static/js/runtime-main.3cab93c1.js"
+  }
+]);
